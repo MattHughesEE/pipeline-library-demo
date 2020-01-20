af1475a7cadfd52d5513cba9b5dda78482b2861e
MattHughesEE <39420229+MattHughesEE@users.noreply.github.com>
2020-01-20 12:26:18 +0800

    Create qualityGate.groovy

---
+++ qualityGate.groovy
@@ -0,0 +1,16 @@
+def call(Map stageParams) {
+  pipeline {
+    agent {
+      label 'master' // label [ label: 'stageParams.label']
+    }
+    stages {
+      stage ('qualityGate') {
+        steps {
+          timeout(time: 2, unit: 'MINUTES') {
+          // Parameter indicates whether to set pipeline to UNSTABLE if Quality Gate fails
+					// true = set pipeline to UNSTABLE, false = don't
+            waitforQualityGate abortPipeline: true
+           }
+          }
+         }
+         
