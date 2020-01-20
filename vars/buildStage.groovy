6dd0d78708028f630ecb8fff6d28a06a0b4fce96
MattHughesEE <39420229+MattHughesEE@users.noreply.github.com>
2020-01-20 12:12:44 +0800

    Update buildStage.groovy

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -1,3 +1,33 @@
-def call(Map pipelineParams)
+def call(Map stageParams) {
   pipeline {
-    agent
+    agent {
+      label 'master' // label [ label: 'stageParams.label']
+    }
+    stages {
+      stage ('gitCheckout') {
+      steps {
+        checkout([
+    $class: 'GitSCM', 
+    branches: [[name: 'stageParams.branch']],
+    doGenerateSubmoduleConfigurations: false,
+    userRemoteConfigs: [[url: 'stageParams.scmUrl']]
+    ])
+      }
+    }
+      stage('build & SonarQube Analysis') {
+        sh label 'setting new build number', script: 'mvn versions:set -DnewVersion=${BUILD_ID}-RELEASE'
+        withSonarQubeEnv('SonarQube') {
+          sh 'mvn clean package sonar:sonar'
+        }
+      }
+      stash includes: 'target/*.jar', name: 'app'
+      stash includes: 'Dockerfile', name: 'dockerfile'
+      stash includes: 'encryption.sh', name: 'entrypoint'
+    }
+  }
+}
+    
+            
+  //sh [label: 'stageParams.label']
+  
+
